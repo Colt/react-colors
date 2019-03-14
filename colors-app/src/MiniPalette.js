ae1533981bf469700eea96de9cf23231950d0790
Colt Steele <colt@steele.com>
2019-03-13 21:13:34 -0700

    Start styling MiniPalette

--- colors-app/src/MiniPalette.js
+++ colors-app/src/MiniPalette.js
@@ -2,34 +2,46 @@ import React from "react";
 import { withStyles } from "@material-ui/styles";
 
 const styles = {
-  main: {
-    backgroundColor: "purple",
-    border: "3px solid teal"
-  },
-  secondary: {
-    backgroundColor: "pink",
-    "& h1": {
-      color: "white",
-      "& span": {
-        backgroundColor: "yellow"
-      }
+  root: {
+    backgroundColor: "white",
+    border: "1px solid black",
+    borderRadius: "5px",
+    padding: "0.5rem",
+    position: "relative",
+    overflow: "hidden",
+    "&:hover": {
+      cursor: "pointer"
     }
+  },
+  colors: {
+    backgroundColor: "grey"
+  },
+  title: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "0",
+    color: "black",
+    paddingTop: "0.5rem",
+    fontSize: "1rem",
+    position: "relative"
+  },
+  emoji: {
+    marginLeft: "0.5rem",
+    fontSize: "1.5rem"
   }
 };
 
 function MiniPalette(props) {
-  const { classes } = props;
+  const { classes, paletteName, emoji } = props;
   console.log(classes);
 
   return (
-    <div className={classes.main}>
-      <h1>Mini Palette</h1>
-      <section className={classes.secondary}>
-        <h1>
-          Mini Palette <span>ajskhd</span>
-        </h1>
-        <span>ajskhd</span>
-      </section>
+    <div className={classes.root}>
+      <div className={classes.colors} />
+      <h5 className={classes.title}>
+        {paletteName} <span className={classes.emoji}>{emoji}</span>
+      </h5>
     </div>
   );
 }
--- colors-app/src/PaletteList.js
+++ colors-app/src/PaletteList.js
@@ -10,9 +10,7 @@ class PaletteList extends Component {
         <MiniPalette />
         <h1>React Colors</h1>
         {palettes.map(palette => (
-          <p>
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-          </p>
+          <MiniPalette {...palette} />
         ))}
       </div>
     );
