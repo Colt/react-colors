4c49f8b62402a9acfc81435434ba6ba23d49a201
Colt Steele <colt@steele.com>
2019-03-18 15:52:22 -0700

    Tweak styles of form

--- colors-app/src/constants.js
+++ colors-app/src/constants.js
@@ -1,1 +1,1 @@
-export const DRAWER_WIDTH = 800;
+export const DRAWER_WIDTH = 400;
--- colors-app/src/styles/NewPaletteFormStyles.js
+++ colors-app/src/styles/NewPaletteFormStyles.js
@@ -5,9 +5,6 @@ const styles = theme => ({
   root: {
     display: "flex"
   },
-  hide: {
-    display: "none"
-  },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
@@ -21,6 +18,7 @@ const styles = theme => ({
   drawerHeader: {
     display: "flex",
     alignItems: "center",
+    width: "100%",
     padding: "0 8px",
     ...theme.mixins.toolbar,
     justifyContent: "flex-end"
@@ -28,7 +26,7 @@ const styles = theme => ({
   content: {
     flexGrow: 1,
     height: "calc(100vh - 64px)",
-    padding: theme.spacing.unit * 3,
+    padding: 0,
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen
