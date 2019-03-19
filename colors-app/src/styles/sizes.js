ff412b68bc362b7efe23e0b75824aa6bb78080f0
Colt Steele <colt@steele.com>
2019-03-19 14:18:29 -0700

    Add responsive helper and style ColorBox

---
+++ colors-app/src/styles/sizes.js
@@ -0,0 +1,12 @@
+export default {
+  up() {},
+  down(size) {
+    const sizes = {
+      xs: "575.98px",
+      sm: "767.98px",
+      md: "991.98px",
+      lg: "1199.98px"
+    };
+    return `@media (max-width: ${sizes[size]})`;
+  }
+};
