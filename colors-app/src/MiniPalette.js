f3ee970d61f2106f7c631d99b2c7a677e6b7f476
Colt Steele <colt@steele.com>
2019-03-18 22:00:20 -0700

    Add deleteIcon to MiniPalette

--- colors-app/src/MiniPalette.js
+++ colors-app/src/MiniPalette.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { withStyles } from "@material-ui/styles";
 import styles from "./styles/MiniPaletteStyles";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function MiniPalette(props) {
   const { classes, paletteName, emoji, colors } = props;
@@ -14,6 +15,12 @@ function MiniPalette(props) {
 
   return (
     <div className={classes.root} onClick={props.handleClick}>
+      <div className={classes.delete}>
+        <DeleteIcon
+          className={classes.deleteIcon}
+          style={{ transition: "all 0.3s ease-in-out" }}
+        />
+      </div>
       <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
--- colors-app/src/styles/MiniPaletteStyles.js
+++ colors-app/src/styles/MiniPaletteStyles.js
@@ -6,8 +6,9 @@ export default {
     padding: "0.5rem",
     position: "relative",
     overflow: "hidden",
-    "&:hover": {
-      cursor: "pointer"
+    cursor: "pointer",
+    "&:hover svg": {
+      opacity: 1
     }
   },
   colors: {
@@ -38,5 +39,18 @@ export default {
     margin: "0 auto",
     position: "relative",
     marginBottom: "-3.5px"
+  },
+  delete: {},
+  deleteIcon: {
+    color: "white",
+    backgroundColor: "#eb3d30",
+    width: "20px",
+    height: "20px",
+    position: "absolute",
+    right: "0px",
+    top: "0px",
+    padding: "10px",
+    zIndex: 10,
+    opacity: 0
   }
 };
