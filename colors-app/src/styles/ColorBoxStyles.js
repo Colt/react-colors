8a5623f1d194d06d6b81253151e9377b1f8f2ebd
Colt Steele <colt@steele.com>
2019-03-19 14:30:04 -0700

    Make ColorBox responsive

--- colors-app/src/styles/ColorBoxStyles.js
+++ colors-app/src/styles/ColorBoxStyles.js
@@ -14,11 +14,11 @@ export default {
     },
     [sizes.down("lg")]: {
       width: "25%",
-      height: props => (props.showingFullPalette ? "20%" : "50%")
+      height: props => (props.showingFullPalette ? "20%" : "33.3333%")
     },
     [sizes.down("md")]: {
       width: "50%",
-      height: props => (props.showingFullPalette ? "10%" : "50%")
+      height: props => (props.showingFullPalette ? "10%" : "20%")
     },
     [sizes.down("xs")]: {
       width: "100%",
@@ -115,7 +115,10 @@ export default {
       textAlign: "center",
       marginBottom: "0",
       padding: "1rem",
-      textTransform: "uppercase"
+      textTransform: "uppercase",
+      [sizes.down("xs")]: {
+        fontSize: "6rem"
+      }
     },
     "& p": {
       fontSize: "2rem",
