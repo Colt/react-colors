39bb6d9693594a0edbed8cf6de281d1290a05acc
Colt Steele <colt@steele.com>
2019-03-19 18:00:49 -0700

    Add bg svg to PaletteList

--- colors-app/src/styles/PaletteListStyles.js
+++ colors-app/src/styles/PaletteListStyles.js
@@ -1,11 +1,18 @@
 import sizes from "./sizes";
+import bg from "./bg.svg";
 export default {
   root: {
-    backgroundColor: "blue",
     height: "100vh",
     display: "flex",
     alignItems: "flex-start",
-    justifyContent: "center"
+    justifyContent: "center",
+    /* background by SVGBackgrounds.com */
+    backgroundColor: "#394bad",
+    backgroundImage: `url(${bg})`,
+    overflow: "scroll"
+  },
+  heading: {
+    fontSize: "2rem"
   },
   container: {
     width: "50%",
@@ -41,7 +48,7 @@ export default {
     },
     [sizes.down("xs")]: {
       gridTemplateColumns: "repeat(1, 100%)",
-      gridGap: "1rem"
+      gridGap: "1.4rem"
     }
   }
 };
